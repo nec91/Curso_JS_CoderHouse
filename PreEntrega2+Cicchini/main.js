52a2d2f5a614f28e9139a7b950f8ead4b5773615
Nicolas Cicchini <nicolase.cicchini@gmail.com>
2023-08-01 21:32:43 -0300

    final

--- PreEntrega2+Cicchini/main.js
+++ PreEntrega2+Cicchini/main.js
@@ -1,3 +1,5 @@
+
+//constructor de usuarios
 class User {
     constructor(user, password, name, lastName, age, gender, id) {
         this.user = user
@@ -10,7 +12,7 @@ class User {
     }
 }
 
-
+//usuarios ya predefinidos
 const user1 = new User("omar42", "qwerty25", "Omar", "Perez", 85, "male", 1)
 const user2 = new User("marta55", "qwerty26", "Marta", "Lopez", 70, "female", 2)
 const user3 = new User("pedro44", "qwerty27", "Pedro", "Martinez", 55, "male", 3)
@@ -19,10 +21,13 @@ const user5 = new User("belen43", "qwerty28", "Belen", "Coleccia", 59, "female",
 const user6 = new User("Julio85", "qwerty29", "Julio", "Rodriguez", 57, "male", 6)
 const user7 = new User("sandra77", "qwerty26", "Sandra", "Krivich", 52, "female", 7)
 
+//base de datos como array
 let userDB = [user1, user2, user3, user4, user5, user6, user7]
 
+//array vacio donde se almacena el id del usuario loggeado
 let userLoggedId = []
 
+//funcion para agregar un usuario
 function addUser() {
     let user = prompt("Ingrese un nombre de usuario").toLocaleLowerCase().trim()
     if (userDB.some((i) => i.user === user)) {
@@ -49,6 +54,8 @@ function addUser() {
     userDB.push(newUser)
 }
 
+
+//funcion para loggearse
 function logIn() {
     let userLog = prompt("Bienvenido, ingrese su usuario.").toLocaleLowerCase().trim()
     const logResult = userDB.find(i => i.user === userLog)
@@ -70,7 +77,7 @@ function logIn() {
     }
 }
 
-
+//funcion calculo para genero masculino
 function male() {
     let user = userDB[userLoggedId - 1]
     let aportes = parseInt(prompt("¿Cuantos años de aportes tiene?"))
@@ -87,6 +94,8 @@ function male() {
     }
 }
 
+
+//funcion calculo para genero femenino
 function female() {
     let user = userDB[userLoggedId - 1]
     let aportes = parseInt(prompt("¿Cuantos años de aportes tiene?"))
@@ -103,7 +112,7 @@ function female() {
     }
 }
 
-
+//funcion que combina calculo de ambos generos y ejecuta dependiendo del mismo
 function calculator() {
     let user = userDB[userLoggedId - 1]
     if (user.gender == "male") {
@@ -113,3 +122,18 @@ function calculator() {
     }
 }
 
+function start() {
+    let welcome = confirm(`¡Bienvenido! ¿Usted se encuentra registrado? Responda "Aceptar" por si, o "Cancelar" por no. `)
+    console.log(welcome)
+    switch (welcome) {
+        case false:
+            alert("Regístrese a continuación")
+            addUser()
+            break
+        case true:
+            logIn()
+        case true:
+            calculator()
+            break
+    }
+}
